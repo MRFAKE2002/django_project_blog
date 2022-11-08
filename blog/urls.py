a227aae11d8b575691afcdf202594fa9ed788d32
ROOZBEH <MR.FAKE@gmail.com>
2022-11-08 14:30:45 +0330

    add create and update and delete view and add from in templates

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='blog_list' ),
     path('<int:pk>/', views.BlogDetailView.as_view(), name='blog_detail' ),
+    path('create/', views.BlogCreateView.as_view(), name='blog_create' ),
+    path('<int:pk>/update/', views.BlogUpdateView.as_view(), name='blog_update' ),
+    path('<int:pk>/delete/', views.BlogDeleteView.as_view(), name='blog_delete' ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views import generic 
+from django.urls import reverse_lazy
 
 from .models import Blog
+from .forms import BlogPostForm
 
 class BlogListView(generic.ListView):
     # model = Blog
@@ -15,4 +17,22 @@ class BlogDetailView(generic.DetailView):
     template_name = 'blog/blog_detail.html'
     context_object_name = 'post'
 
+
+class BlogCreateView(generic.CreateView):
+    model = Blog
+    form_class = BlogPostForm
+    template_name = 'blog/blog_create.html'
+
+
+class BlogUpdateView(generic.UpdateView):
+    model = Blog
+    form_class = BlogPostForm
+    template_name = 'blog/blog_create.html'
     
+
+class BlogDeleteView(generic.DeleteView):
+    model = Blog
+    template_name = 'blog/blog_delete.html'
+    success_url = reverse_lazy('blog_list')
+    
+

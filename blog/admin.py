1663fcd9bf0b699a0737fc19d9366e3e4944849a
ROOZBEH <MR.FAKE@gmail.com>
2022-11-10 21:42:46 +0330

    show PostComment into admin site and blog_detail

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Blog
+from .models import Blog, PostComment
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "status")
+
+
+@admin.register(PostComment)
+class PostCommentAdmin(admin.ModelAdmin):
+    list_display = ("post", "author", "body", "stars")
 
